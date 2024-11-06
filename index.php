b9176e5bcf73756e75e7f1779b6a52796544499a
unknown <vinicius.ritz@alunos.fibbauru.br>
2024-11-05 21:27:15 -0300

    Adicionando index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h2>Cadastro Agronomia</h2>
+	<a href="html/tabelaProp.php"><button>Propriedade</button></a>
+	<br><br>
+	<a href="html/tabelaCult.php"><button>Culturas</button></a>
+	<br><br>
+	<a href="html/tabelaEquip.php"><button>Equipamentos</button></a>
+	<br><br>
+</body>
+</html>
